8bb8c8846d7a8bdf912358711496989bd16f93dc
EstebanForero <foreroesteban93@gmail.com>
2026-03-26 21:13:30 -0500

    favoritos modificado

--- catalogo-peliculas/src/pages/Favoritos.jsx
+++ catalogo-peliculas/src/pages/Favoritos.jsx
@@ -1,30 +1,41 @@
-const PeliculasLista = ({ peliculas, onSelect, onDelete }) => {
+import { useEffect, useState } from "react";
+import { obtenerFavoritos, eliminarFavorito } from "../utils/localStorage";
+import PeliculasLista from "../components/PeliculasLista";
+import { useNavigate, useLocation } from "react-router-dom";
+
+function Favoritos() {
+  const [favoritos, setFavoritos] = useState([]);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const data = obtenerFavoritos();
+    console.log("FAVORITOS CARGADOS:", data); // debug
+    setFavoritos(data);
+  }, [location]);
+
+  const handleDelete = (id) => {
+    eliminarFavorito(id);
+    setFavoritos(favoritos.filter(f => f.imdbID !== id));
+  };
+
   return (
-    <div className="grid">
-      {Array.isArray(peliculas) &&
-        peliculas.map((peli) => (
-          <div className="card" key={peli.imdbID}>
-            <img
-              src={
-                peli.Poster !== "N/A"
-                  ? peli.Poster
-                  : "https://via.placeholder.com/200"
-              }
-              alt={peli.Title}
-              onClick={() => onSelect(peli.imdbID)}
-            />
+    <div>
+      <h2>Favoritos</h2>
 
-            <h3>{peli.Title}</h3>
-
-            {onDelete && (
-              <button onClick={() => onDelete(peli.imdbID)}>
-                Eliminar
-              </button>
-            )}
-          </div>
-        ))}
+      {favoritos.length === 0 ? (
+        <p style={{ marginLeft: "20px" }}>
+          No hay películas en favoritos 😢
+        </p>
+      ) : (
+        <PeliculasLista
+          peliculas={favoritos}
+          onSelect={(id) => navigate(`/detalle/${id}`)}
+          onDelete={handleDelete}
+        />
+      )}
     </div>
   );
-};
+}
 
-export default PeliculasLista;
+export default Favoritos;
